6b21a0d746a1448e0ebb4bebe2215f62fba57153
naruto678 <arnabthakuria678@gmail.com>
2022-11-24 19:35:08 +0530

    second stage

--- app/main.go
+++ app/main.go
@@ -8,19 +8,19 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-  
-	fmt.Println("Logs from your program will appear here!")
 
-	
 	command := os.Args[3]
+
 	args := os.Args[4:len(os.Args)]
-  
-	 cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+
+	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
-	fmt.Printf("Err: %v", err)
+		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-	
-	fmt.Println(string(output))
+
 }
